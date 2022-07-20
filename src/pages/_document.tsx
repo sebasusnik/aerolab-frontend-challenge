7268219bd4620b6155855a0c26a211b9db5ce6ee
sebasusnik <sebasusnik@gmail.com>
2022-07-20 16:37:27 -0300

    metadata correction

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -14,29 +14,26 @@ export default function Document() {
 
         {/* -- Open Graph / Facebook -- */}
         <meta property="og:type" content="website" />
-        <meta property="og:url" content="https://metatags.io/" />
+        <meta property="og:url" content="https://aerolab-frontend-challenge-theta.vercel.app" />
         <meta property="og:title" content="Aerolab - DEVELOPER CHALLENGE" />
         <meta
           property="og:description"
           content="This is my resolution of the aerolab frontend developer challenge. If you like my work please contact me as /sebasusnik en linkedin or github."
         />
-        <meta
-          property="og:image"
-          content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png"
-        />
+        <meta property="og:image" content="/illustrations/Metadata.png" />
 
         {/* -- Twitter -- */}
-        <meta property="twitter:card" content="summary_large_image" />
-        <meta property="twitter:url" content="https://metatags.io/" />
+        <meta property="twitter:card" content="/illustrations/Metadata.png" />
+        <meta
+          property="twitter:url"
+          content="https://aerolab-frontend-challenge-theta.vercel.app"
+        />
         <meta property="twitter:title" content="Aerolab - DEVELOPER CHALLENGE" />
         <meta
           property="twitter:description"
           content="This is my resolution of the aerolab frontend developer challenge. If you like my work please contact me as /sebasusnik en linkedin or github."
         />
-        <meta
-          property="twitter:image"
-          content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png"
-        />
+        <meta property="twitter:image" content="/illustrations/Metadata.png" />
       </Head>
       <body>
         <Main />
