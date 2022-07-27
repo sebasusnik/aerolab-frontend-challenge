7542a249d3c3dfdc2f68591c08559ad9ed91af21
sebasusnik <sebasusnik@gmail.com>
2022-07-26 22:24:55 -0300

    meta tags

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,7 +5,9 @@ export default function Document() {
     <Html>
       <Head>
         {/* -- Primary Meta Tags -- */}
-
+        <meta charSet="UTF-8" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         {/* -- Open Graph / Facebook -- */}
         <meta property="og:type" content="website" />
         <meta property="og:url" content="https://aerolab-frontend-challenge-theta.vercel.app" />
