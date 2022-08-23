927aa472e8258ee3fa1ca238437bcc6a2b79a12c
sebasusnik <sebasusnik@gmail.com>
2022-08-22 22:35:19 -0300

    product images

--- src/product/ProductCard.tsx
+++ src/product/ProductCard.tsx
@@ -1,8 +1,11 @@
-import React from 'react';
+import React, {useState} from 'react';
+
+import Image from 'next/image';
 
 import {Product} from './types';
 import CTA from '~layout/components/CTA';
 import {ButtonVariants} from '~layout/types';
+import imagePlaceholder from '~public/logo-placeholder.png';
 import {usePoints} from '~user/hooks';
 
 interface Props {
@@ -10,6 +13,8 @@ interface Props {
 }
 
 const ProductCard: React.FC<Props> = ({product}) => {
+  const [isImageLoading, setIsImageLoading] = useState(true);
+
   const [points] = usePoints();
 
   return (
@@ -29,8 +34,30 @@ const ProductCard: React.FC<Props> = ({product}) => {
           <h3 className="text-lg">{product.name}</h3>
           <p className="text-md allcaps secondary">{product.category}</p>
         </div>
-        <div className="product-image-holder">
-          <img src="" alt="" />
+        <div className="product-card-image-wrapper">
+          {isImageLoading && (
+            <div className="product-card-image-placeholder">
+              <Image
+                src={imagePlaceholder}
+                alt={'loading image'}
+                height={72}
+                objectFit={'cover'}
+                width={78}
+                className={'product-card-image'}
+              />
+            </div>
+          )}
+          <div className="product-card-image-holder">
+            <Image
+              src={product.img.hdUrl}
+              alt=""
+              width={504}
+              layout="fill"
+              objectFit="contain"
+              className="product-card-image"
+              onLoadingComplete={() => setIsImageLoading(true)}
+            />
+          </div>
         </div>
       </div>
     </div>
