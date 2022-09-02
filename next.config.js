da35effe7dfde0f84295dccee59ecfc1caa7c8bf
sebasusnik <sebasusnik@gmail.com>
2022-09-01 21:33:41 -0300

    change browser suport

--- next.config.js
+++ next.config.js
@@ -1,9 +1,27 @@
+const isProd = process.env.NODE_ENV === 'production';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: ['coding-challenge-api.aerolab.co'],
   },
+  webpack: (config, {isServer, dev}) => {
+    if (!isServer && dev) {
+      config.optimization.minimize = false;
+      delete config.optimization.minimizer;
+      config.target = ['web', 'es2020'];
+    }
+
+    return config;
+  },
 };
+
+if (!isProd) {
+  nextConfig.experimental = {
+    legacyBrowsers: false,
+    browsersListForSwc: true,
+  };
+}
 
 module.exports = nextConfig;
