568c1f12ad73f3ea386d165368564dabc9b1c9db
sebasusnik <sebasusnik@gmail.com>
2022-12-05 16:40:12 -0300

    product redeem funciton

--- src/product/ProductCard.tsx
+++ src/product/ProductCard.tsx
@@ -6,7 +6,7 @@ import {Product} from './types';
 import CTA from '~layout/components/CTA';
 import {ButtonVariants} from '~layout/types';
 import imagePlaceholder from '~public/logo-placeholder.png';
-import {usePoints} from '~user/hooks';
+import {usePoints, useRedeem} from '~user/hooks';
 
 interface Props {
   product: Product;
@@ -16,6 +16,14 @@ const ProductCard: React.FC<Props> = ({product}) => {
   const [isImageLoading, setIsImageLoading] = useState(true);
 
   const [points] = usePoints();
+  const canBuy = product.cost <= points;
+  const redeem = useRedeem();
+
+  function handleRedeem() {
+    if (canBuy) {
+      return redeem(product);
+    }
+  }
 
   return (
     <div className="product-card-wrapper flex">
@@ -26,11 +34,11 @@ const ProductCard: React.FC<Props> = ({product}) => {
       >
         {points ? (
           <CTA
-            variant={product.cost < points ? ButtonVariants.Default : ButtonVariants.Disable}
+            variant={canBuy ? ButtonVariants.Default : ButtonVariants.Disable}
             left={product.cost < points ? 'Redeem for' : 'You need'}
             right={product.cost.toString()}
             type="button"
-            action={() => console.log('hehe')}
+            action={handleRedeem}
           />
         ) : (
           <CTA variant={ButtonVariants.Skeleton} />
